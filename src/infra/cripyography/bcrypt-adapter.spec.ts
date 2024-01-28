86e17af58dcfc8b79732c75b0c0f0c2c6d740d73
David Borelli <david_borelli@live.com>
2024-01-28 11:07:33 -0300

    test: ensure BcryptAdapter throws if bcrypt throw

--- src/infra/cripyography/bcrypt-adapter.spec.ts
+++ src/infra/cripyography/bcrypt-adapter.spec.ts
@@ -27,4 +27,13 @@ describe('BCrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  test('Shoud throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
